68888e1432bf5f6197c6e9f7ed4d5eb14329a65c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-10-02 19:36:56 +0700

    Change name

--- Scripts/Delegates/RequestDelegate.cs
+++ Scripts/Delegates/RequestDelegate.cs
@@ -1,4 +1,4 @@
 namespace FishNet.Insthync.ResquestResponse
 {
-    public delegate void RequestDelegate<TRequest, TResponse>(RequestHandlerData requestHandler, TRequest request, RequestProceedResultDelegate<TResponse> responseProceedResult);
+    public delegate void RequestDelegate<TRequest, TResponse>(RequestHandlerData requestHandler, TRequest request, RequestProceedResultDelegate<TResponse> result);
 }
