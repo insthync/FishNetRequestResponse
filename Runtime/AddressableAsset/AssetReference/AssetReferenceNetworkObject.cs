e1f0b95edf5e3e5ca2f138447882ace7b9e7c0b0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-05-05 18:24:16 +0700

    feat: add asset reference to network object

---
+++ Runtime/AddressableAsset/AssetReference/AssetReferenceNetworkObject.cs
@@ -0,0 +1,12 @@
+using FishNet.Object;
+using Insthync.AddressableAssetTools;
+
+namespace FishNet.Insthync.AddressableAsset
+{
+    public class AssetReferenceNetworkObject : AssetReferenceComponent<NetworkObject>
+    {
+        public AssetReferenceNetworkObject(string guid) : base(guid)
+        {
+        }
+    }
+}
